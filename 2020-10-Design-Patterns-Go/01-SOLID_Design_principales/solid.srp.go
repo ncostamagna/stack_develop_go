2f8e01b1bc70a9bab8e9086804ea4ee7dff0862f
Nahuel Costamagna <ncostamagna@digitalhouse.com>
2020-09-21 23:58:57 -0300

    FEAT-000:add design patterns course

---
+++ 2020-10-Design-Patterns-Go/01-SOLID_Design_principales/solid.srp.go
@@ -0,0 +1,83 @@
+package SOLID_Design_principales
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"strings"
+)
+
+var entryCount = 0
+
+type Journal struct {
+	entries []string
+}
+
+func (j *Journal) String() string {
+	return strings.Join(j.entries, "\n")
+}
+
+// responsabilidad para agregar entidades, Single Responsability Principle,
+// tiene la responsabilidad de agregar
+func (j *Journal) AddEntry(text string) int {
+	entryCount++
+	entry := fmt.Sprintf("%d: %s",
+		entryCount,
+		text)
+	j.entries = append(j.entries, entry)
+	return entryCount
+}
+
+// responsabilidad para quitar entidades, Single Responsability Principle,
+// tiene la responsabilidad de quitar
+func (j *Journal) RemoveEntry(index int) {
+	// ...
+}
+
+// breaks srp
+// preocupaciones separadas, con respecto a las anteriores
+
+func (j *Journal) Save(filename string) {
+	_ = ioutil.WriteFile(filename,
+		[]byte(j.String()), 0644)
+}
+
+func (j *Journal) Load(filename string) {
+
+}
+
+func (j *Journal) LoadFromWeb(url *url.URL) {
+
+}
+
+var lineSeparator = "\n"
+
+func SaveToFile(j *Journal, filename string) {
+	_ = ioutil.WriteFile(filename,
+		[]byte(strings.Join(j.entries, lineSeparator)), 0644)
+}
+
+type Persistence struct {
+	lineSeparator string
+}
+
+func (p *Persistence) saveToFile(j *Journal, filename string) {
+	_ = ioutil.WriteFile(filename,
+		[]byte(strings.Join(j.entries, p.lineSeparator)), 0644)
+}
+
+func main_() {
+	j := Journal{}
+	j.AddEntry("I cried today.")
+	j.AddEntry("I ate a bug")
+	fmt.Println(strings.Join(j.entries, "\n"))
+
+	// separate function
+	SaveToFile(&j, "journal.txt")
+
+	// le agrego el caracter que utilizaremos para persistir
+	// de una manera separada
+	// la principal responsabilidad es de la clase
+	p := Persistence{"\n"}
+	p.saveToFile(&j, "journal.txt")
+}
